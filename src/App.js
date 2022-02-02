aa99db263830a542c695a745902b21c0aaed603c
ari-harlemcaballero <ari.bash.harlem@gmail.com>
2022-02-01 17:35:10 -0800

    app layout set

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { useState } from 'react';
 import './App.css';
+import orderImages from './components/orderImages';
+import foodDropdown from './components/Dropdowns/foodDropdown';
+import drinkDropdown from './components/Dropdowns/drinkDropdown';
+import sideDropdown from './components/Dropdowns/sideDropdown';
+import InstructionList from './components/Instructions/InstructionList';
+import instructionForm from './components/Instructions/instructionForm';
+import orderName from './components/orderName';
 
 function App() {
   const [foodId, setFoodId] = useState(1);
   const [sideId, setSideId] = useState(1);
   const [drinkId, setDrinkId] = useState(1);
   const [orderName, setOrderName] = useState('Your Name Here');
-  const [instructions, setInstructions] = useState(['no pickles', 'extra fries']);
+  const [instructionsArr, setInstructionsArr] = useState(['no pickles', 'extra fries']);
 
   return (
     <div className="App">
@@ -17,6 +24,14 @@ function App() {
           foodId={foodId}
           sideId={sideId}
           drinkId={drinkId} />
+        <div className='dropdowns'>
+          <foodDropdown setFoodId={setFoodId} />
+          <sideDropdown setSideId={setSideId} />
+          <drinkDropdown setDrinkId={setDrinkId} />
+        </div>
+        <orderName setOrderName={setOrderName} />
+        <instructionForm setInstructionsArr={setInstructionsArr} instructionsArr={instructionsArr} />
+        <InstructionList instructionsArr={instructionsArr} />
       </section>
     </div>
   );
