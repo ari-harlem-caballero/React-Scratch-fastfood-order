b89825a5c47628c1f195b7d462211ad721936295
ari-harlemcaballero <ari.bash.harlem@gmail.com>
2022-02-01 17:10:03 -0800

    added state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [foodId, setFoodId] = useState(1);
+  const [sideId, setSideId] = useState(1);
+  const [drinkId, setDrinkId] = useState(1);
+  const [orderName, setOrderName] = useState('Your Name Here');
+  const [instructions, setInstructions] = useState(['no pickles', 'extra fries']);
+
   return (
     <div className="App">
      App Blah Blah
