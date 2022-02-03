f074ee79a47d59a35d901754025172d787e7e991
ari-harlemcaballero <ari.bash.harlem@gmail.com>
2022-02-02 17:21:18 -0800

    fixed test error

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Order for/i);
+  const linkElement = screen.getByText(/Main:/i);
   expect(linkElement).toBeInTheDocument();
 });
